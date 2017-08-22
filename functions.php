723562e44303574e109bb52e4846fcb663e2efb2
Cliff Seal <cliff@logos-creative.com>
2017-08-22 08:59:18 -0400

    Remove header

--- functions.php
+++ functions.php
@@ -44,6 +44,12 @@ genesis_unregister_layout( 'content-sidebar-sidebar' );
 genesis_unregister_layout( 'sidebar-content-sidebar' );
 genesis_unregister_layout( 'sidebar-sidebar-content' );
 
+// Remove header
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+
 // Remove breadcrumbs
 remove_action('genesis_before_loop', 'genesis_do_breadcrumbs');
 
